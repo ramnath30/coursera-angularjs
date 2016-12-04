3fa2caa2bbef10408f7c245576f358191f637a5b
drams <ramnath30@yahoo.com>
2016-12-04 17:43:13 -0600

    Assignment 2

---
+++ assignment2/app.js
@@ -0,0 +1,40 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  this.items = ShoppingListCheckOffService.toBuyItems;
+  this.buy = function(index) {
+    ShoppingListCheckOffService.buy(index);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  this.items = ShoppingListCheckOffService.alreadyBoughtItems;
+}
+
+function ShoppingListCheckOffService() {
+  this.toBuyItems = [
+    {name: "Apples", quantity: "5"},
+    {name: "Bananas", quantity: "3"},
+    {name: "Carrots", quantity: "10"},
+    {name: "Daikons", quantity: "3"},
+    {name: "Endives", quantity: "10"}
+  ];
+
+  this.alreadyBoughtItems = [];
+
+  this.buy = function(index) {
+    var newItem = {name: this.toBuyItems[index].name, quantity: this.toBuyItems[index].quantity};
+    this.toBuyItems.splice(index, 1);
+    this.alreadyBoughtItems.push(newItem);
+  }
+
+}
+})();
